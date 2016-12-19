b5137dd24d9664811c389a4a6f8c707282d0e99d
Akash Yadav <akashyadav20111992@gmail.com>
2016-12-20 01:50:04 +0530

    ko is not defined error

---
+++ app.js
@@ -0,0 +1,9 @@
+var ViewModel = function(){
+  this.name = ko.observable(halo);
+  this.clicks = ko.observable(0);
+  this.imgSrc = ko.observable("cat1.jpg");
+  this.incrementCounter = function(){
+    this.clicks(this.clicks()+1);
+  }
+}
+ko.applyBindings(new ViewModel());
