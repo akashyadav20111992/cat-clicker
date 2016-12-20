76cdc614669e8e1813b5f3f181ac50de3e61b88b
Akash Yadav <akashyadav20111992@gmail.com>
2016-12-21 00:23:29 +0530

    with binding problem

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var ViewModel = function(){
+var cat = function(){
   this.name = ko.observable("halo");
   this.clicks = ko.observable(0);
   this.imgSrc = ko.observable("cat1.jpg");
+  this.level = ko.computed(function(){
+    if(this.clicks() <= 10){
+      return "A";
+    }
+    else if (this.clicks() > 10) {
+      return "B";
+    }
+  }, this);
+  this.nicknames = ko.observableArray([
+    {name:"nene"},
+    {name:"nini"},
+    {name:"nono"},
+    {name:"nunu"}
+  ]);
+}
+var ViewModel = function(){
+  this.currentCat = ko.observable(new cat());
   this.incrementCounter = function(){
-    this.clicks(this.clicks()+1);
+    this.currentCat().clicks(this.currentCat().clicks()+1);
   }
 }
 ko.applyBindings(new ViewModel());
